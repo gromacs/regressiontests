67818653da295c51174d8ba087bb5723248ae0b7
Roland Schulz <roland@utk.edu>
2014-03-03 18:05:19 -0500

    Report check tpr file names
    Allows to check cause of error.
    
    Change-Id: I47d9750be4a0fb994762701a826bb08a8f87c5b9

--- gmxtest.pl
+++ gmxtest.pl
@@ -431,13 +431,15 @@ sub test_systems {
 			sub { print "Comparison of input .tpr files failed!\n"; $nerror = 1; });
 		    $nerror |= find_in_file("step","$tprout");
 		    if ($nerror > 0) {
+			push(@error_detail, ("checktpr.out", "checktpr.err"));
 			print "topol.tpr file different from $reftpr. Check files in $dir\n";
+		    } else {
+			unlink($tprout,$tprerr);
 		    }
 		    if (find_in_file ('reading tpx file (reference_[sd].tpr) version .* with version .* program',"$tprout") > 0) {
 			print "\nThe GROMACS version being tested may be older than the reference version.\nPlease see the note at end of this output.\n";
 			$addversionnote = 1;
 		    }
-		    unlink($tprout,$tprerr);
 		}
 	    }
 	    if ($nerror == 0) {
