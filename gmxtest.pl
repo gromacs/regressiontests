eae86351900755007504bad778ed9c817661cd9d
Szilard Pall <szilard.pall@gmail.com>
2014-10-10 03:27:01 +0200

    Respect rank/thread maximum in CPU reruns
    CPU-only reruns on GPU-enabled tets didn't respect MPI rank and OpenMP
    thread count limits.
    
    Fixes #1624
    
    Change-Id: I58210e37129d725ef2fe2e7b1ff3c2b5ecfb5d74

--- gmxtest.pl
+++ gmxtest.pl
@@ -828,6 +828,16 @@ sub test_systems {
                 $input_dir = "..";
                 $test_name .= "-cpu-only";
                 mkdir $dir;
+                my $limiter_file = $dir . '/' . $input_dir . '/max-mpi-ranks';
+                if (-e $limiter_file) {
+                    print( '~~~> ' . $ENV{'PWD'} . "\n");
+                    copy($limiter_file, $dir);
+                }
+                $limiter_file = $dir . '/' . $input_dir . '/max-openmp-threads';
+                if (-e $limiter_file) {
+                    copy($limiter_file, $dir);
+                }
+
                 $$nn++;
                 $$npassed += test_case $dir, $input_dir, $test_name;
             }
